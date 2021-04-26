649f3a6465f06aff6d74f5c401cd42498b741c08
AhShujah <ahmad.shujah.18@cnu.edu>
2021-04-25 22:34:59 -0400

    z

--- src/PlugNPlay.java
+++ src/PlugNPlay.java
@@ -1,20 +1,13 @@
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
-import dataManipulation.FileRead;
-import dataManipulation.FileSave;
+
 import frameGUIs.MenuGUI;
 
 public class PlugNPlay {
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
-		//FileRead p = new FileRead("src/gameFiles/DataSavingTemplate.txt");
-		
-		//FileSave l = new FileSave("src/gameFiles/testwrite.txt");
-		//l.SaveData(p.getTemplates());
-		
-		MenuGUI menu = new MenuGUI();
+		new MenuGUI();
 	}
 
 }
